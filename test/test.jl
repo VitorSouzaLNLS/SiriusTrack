0cdbba18bf640beb2c113568c22d1f40e146309c
Vitor <vitor.souza@lnls.br>
2024-02-13 12:44:27 -0300

    saving

--- test/test.jl
+++
@@ -1,9 +0,0 @@
-using SiriusTrack
-using SiriusTrack.Auxiliary
-
-model = SiriusTrack.Models.StorageRing.create_accelerator()
-model.cavity_state = on
-model.vchamber_state = on
-model.radiation_state = 1
-
-model
